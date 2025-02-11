f4205f3e7efe928d8839ec0bb14ac533fa1b634b
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-11 12:36:29 -0500

    feat: add DGE analysis
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
    
    Former-commit-id: d0ee7842a4398b96c91098b189985fb6de07398c [formerly c50dcbed43d634e73b829c3892512340f84f5a16]
    Former-commit-id: a763f19d4dc1be3c2e091ee7322884cfe907773c

---
+++ ocd_ctrl_dlpfc.R
@@ -0,0 +1,32 @@
+# OCD v CTRL DLPFC
+
+library(tidyverse)
+library(edgeR)
+source("process_dge_fun.R")
+
+counts_file <- file.path("data", "GSE262138_Sample_Counts.csv")
+metadata_file <- file.path("data", "GSE262138_Sample_Metadata.csv")
+
+metadata <- read_csv(metadata_file) |>
+  filter(Diagnosis %in% c("CTRL", "OCD"), Tissue == "DLPFC")
+
+counts <- read_csv(counts_file) |>
+  column_to_rownames("Gene_Symbol") |>
+  select(any_of(metadata$SampleID))
+
+
+groups <- metadata$Diagnosis
+design_matrix <- model.matrix(~ 0 + groups)
+
+dge <- DGEList(counts = counts, group = groups)
+
+keep <- filterByExpr(dge)
+dge_filtered <- dge[keep, , keep.lib.sizes = FALSE]
+
+out_filtered <- process_dge(dge_filtered, design_matrix, num_genes = 1000)
+
+out_filtered$complete_table %>%
+  rownames_to_column("gene_name") %>%
+  write_csv("results/ocd_vs_control_dlpfc.csv")
+
+save(out_filtered, file = "results/ocd_vs_control_dlpfc.RData")
--- process_sample_counts.R
+++ process_sample_counts.R
@@ -1,8 +1,18 @@
 # Read and transform sample count data
 
 library(tidyverse)
+library(org.Hs.eg.db)
 
 sample_count_file <- file.path("extdata", "GSE262138_Raw_count_data.txt")
 
 dataset <- read_tsv(sample_count_file) |>
-    write_csv(file.path("data", "GSE262138_Sample_Counts.csv"))
+  mutate(Geneid = str_remove(Geneid, "\\.\\d+"))
+
+mapped_gene_symbols <- select(org.Hs.eg.db, keys = dataset$Geneid, keytype = "ENSEMBL", columns = c("GENENAME", "SYMBOL"))
+
+annotated_dataset <- dataset |>
+  inner_join(mapped_gene_symbols, by = c(Geneid = "ENSEMBL")) |>
+  filter(!is.na(SYMBOL)) |>
+  summarise(across(where(is.numeric), ~ sum(.x)), .by = "SYMBOL") |>
+  dplyr::select(Gene_Symbol = SYMBOL, starts_with("R")) |>
+  write_csv(file.path("data", "GSE262138_Sample_Counts.csv"))
